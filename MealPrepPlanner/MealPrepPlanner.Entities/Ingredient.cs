016f0f2ee8243d5a37bdafc379cc2035980437aa
Mike Martin <michaelm1687@hotmail.com>
2018-03-23 15:47:19 +0000

    Added Ingredient entity

---
+++ MealPrepPlanner/MealPrepPlanner.Entities/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MealPrepPlanner.Entities
+{
+    public class Ingredient : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string Units { get; set; }
+    }
+}
--- MealPrepPlanner/MealPrepPlanner.Entities/Meal.cs
+++ MealPrepPlanner/MealPrepPlanner.Entities/Meal.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MealPrepPlanner.Entities
 {
     public class Meal : BaseEntity
     {
         public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
     }
 }
